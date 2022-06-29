25d82b569aaebffdc092582c2d4a825e7cc2e2ac
Aleksandr Lyapunov <alyapunov@tarantool.org>
2022-06-29 19:29:25 +0300

    87

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -1,51 +1,72 @@
 #include <alya.h>
-#include <vector>
+#include <array>
+#include <cassert>
 
 using namespace std;
 
-//  Definition for singly-linked list.
-struct ListNode {
-	int val;
-	ListNode *next;
-};
+struct Stat {
+	uint8_t arr[32];
+	void clear()
+	{
+		memset(arr, 0, 32);
+	}
+	void add(char c)
+	{
+		arr[c - 'a']++;
+	}
 
+	bool operator==(const Stat& a) const
+	{
+		return memcmp(arr, a.arr, 32) == 0;
+	}
+};
 
 class Solution {
 public:
-	ListNode* reverseKGroup(ListNode* head, int k) {
+	Stat stat1[32][32];
+	Stat stat2[32][32];
+
+	bool isScramble(const std::string& s1, size_t b1, size_t e1,
+			const std::string& s2, size_t b2, size_t e2)
+	{
+		assert(e1 - b1 == e2 - b2);
+		assert(e1 - b1 > 0);
+		if (e1 - b1 == 1)
+			return s1[b1] == s2[b2];
+		if (!(stat1[b1][e1] == stat2[b2][e2]))
+			return false;
+		for (size_t i = 0; b1 + i < e1; i++) {
+			if (isScramble(s1, b1, b1 + i, s2, b2, b2 + i) &&
+				isScramble(s1, b1 + i, e1, s2, b2 + i, e2))
+				return true;
+			if (isScramble(s1, b1, b1 + i, s2, e2 - i, e2) &&
+			    isScramble(s1, b1 + i, e1, s2, b2 + i, e2))
+				return true;
+		}
+
+	}
+	bool isScramble(const std::string &s1, const std::string &s2) {
+		return isScramble(s1, 0, s1.size(), s2, 0, s2.size());
+
 	}
 };
 
 void
-check(std::initializer_list<int> orig, int k, std::initializer_list<int> res)
+check(const std::string &s1, const std::string &s2, bool exp)
 {
-	std::vector<ListNode> nodes(orig.size());
-	{
-		size_t i = 0;
-		for (int val : orig) {
-			auto &n = nodes[i++];
-			n.val = val;
-			n.next = i == orig.size() ? nullptr : &nodes[i];
-		}
-	}
 	Solution sol;
-	ListNode* r = sol.reverseKGroup(&nodes[0], k);
-	{
-		size_t i = 0;
-		for (int val : res) {
-			if (val != r->val)
-				abort();
-			r = r->next;
-		}
-		if (r != nullptr)
-			abort();
+	bool res = sol.isScramble(s1, s2);
+	if (res != exp) {
+		std::cout << s1 << " and " << s2 << " : "
+			  << "expected " << exp << " got " << res << std::endl;
+		abort();
 	}
 }
 
+
 int main()
 {
-	check({1,2,3,4,5}, 2, {2,1,4,3,5});
-	check({1,2,3,4,5}, 3, {1,2,3,4,5});
-	check({1,2,3,4,5}, 1, {1,2,3,4,5});
-	check({1}, 1, {1});
+	check("great", "rgeat", true);
+	check("abcde", "caebd", false);
+	check("a", "a", true);
 }
